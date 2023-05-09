8001e24358882c9ab485acf4a1dbfbf41ad0f0b9
NetRunner <galatskiy@gmail.com>
2023-05-09 19:11:30 +0300

    Two screens + interface fix + orientation adaptation

---
+++ LoginForm/UserViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UserViewController.swift
+//  LoginForm
+//
+//  Created by Dmitrii Galatskii on 09.05.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
